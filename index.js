8fa504fdf295f62ee0d6442996a2a91450d16d88
adarraji <a.darraji@outlook.com>
2022-08-26 15:31:40 -0400

    added knex db connection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 require('dotenv').config();
+const knex = require("knex");
+
+const db = knex({
+    client: "pg",
+    connection: {
+        host: "127.0.0.1",
+        port: 5432,
+        user: `${process.env.DB_USER}`,
+        password: `${process.env.DB_PASSWORD}`,
+        database: "myapp_test"
+    }
+});
 
 const app = express();
-
 
 app.listen(process.env.PORT || "5000", () => {
     console.log(`app is running on port ${process.env.PORT}`)
