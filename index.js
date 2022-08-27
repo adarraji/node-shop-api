580e2dc438734327b1d381a290e0bce5f3c50937
techbrain2 <techbrain203@outlook.com>
2022-08-27 14:45:51 -0400

    added mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require('dotenv').config();
 const userRoute = require("./routes/user");
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gnkjwte.mongodb.net/shop?retryWrites=true&w=majority`)
+    .then(() => console.log("DB Connection Sucessfull"))
+    .catch((err) => console.log(err));
 
 app.use("/api/users", userRoute);
 
