b176385063c4d00d2c430186761724c090c5e996
techbrain2 <techbrain203@outlook.com>
2022-08-28 12:12:10 -0400

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
     });
 
     try {
