2c82ca661afcf2770f1dc8e67072110a77d7d1c7
adarraji <a.darraji@outlook.com>
2022-08-28 21:59:42 -0400

    updated login

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,21 @@ router.post("/register", async (req, res) => {
 // LOGIN
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne(username, req.body.username)
+        const user = await User.findOne({ username: req.body.username });
+
+        if (!user) {
+            res.status(401).json("Wrong credentials!");
+        } else {
+            const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+            const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+            if (password !== req.body.password) {
+                res.status(401).json("Wrong credentials!");
+            } else {
+                res.status(200).json(user);
+            }
+        }
+
     }
     catch (err) {
         res.status(500).json(err);
