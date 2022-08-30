18251691084a6cbb9a1a768a1ecdc6b1f3ef030b
adarraji <a.darraji@outlook.com>
2022-08-30 19:07:26 -0400

    added get user stats method

--- routes/user.js
+++ routes/user.js
@@ -50,5 +50,32 @@ router.get("/", veryifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+// GET USER STATS
+router.get("/stats", veryifyTokenAndAdmin, async (req, res) => {
+
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    try {
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 }
+                }
+            }
+        ]);
+        res.status(200).json(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
