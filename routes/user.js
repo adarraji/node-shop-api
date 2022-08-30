7d4cc1e9abb19d308363b7fc274ed9f1d316cdf9
adarraji <a.darraji@outlook.com>
2022-08-30 16:37:55 -0400

    update user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,19 @@
+const User = require("../models/User");
 const { veryifyTokenAndAuthorization } = require("./verifyToken");
 const router = require("express").Router();
 
-router.put("/:id", veryifyTokenAndAuthorization, (req, res) => {
+// UPDATE
+router.put("/:id", veryifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(201).json(updatedUser);
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
