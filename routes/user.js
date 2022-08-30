36c329e4aa7f1cbcaad6bbba266a76347ab92b9f
adarraji <a.darraji@outlook.com>
2022-08-30 17:00:27 -0400

    added get all  user method

--- routes/user.js
+++ routes/user.js
@@ -39,4 +39,15 @@ router.get("/find/:id", veryifyTokenAndAdmin, async (req, res) => {
 });
 
 
+// GET ALL USERs
+router.get("/", veryifyTokenAndAdmin, async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 module.exports = router;
