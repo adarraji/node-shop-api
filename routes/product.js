ee98b8e0ea6e200fbae837f280fe15de516a821a
adarraji <a.darraji@outlook.com>
2022-08-31 13:12:41 -0400

    Added update, get, delete

--- routes/product.js
+++ routes/product.js
@@ -15,41 +15,37 @@ router.post("/", veryifyTokenAndAdmin, async (req, res) => {
 })
 
 
-// // UPDATE
-// router.put("/:id", veryifyTokenAndAuthorization, async (req, res) => {
-//     if (req.body.password) {
-//         req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
-//     }
-//     try {
-//         const updatedProduct = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-//         res.status(201).json(updatedUser);
+// UPDATE
+router.put("/:id", veryifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(201).json(updatedProduct);
 
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
-// // DELETE
-// router.delete("/:id", veryifyTokenAndAuthorization, async (req, res) => {
-//     try {
-//         await User.findByIdAndDelete(req.params.id);
-//         res.status(200).json("User has been deleted...");
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+// DELETE
+router.delete("/:id", veryifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
-// // GET USER
-// router.get("/find/:id", veryifyTokenAndAdmin, async (req, res) => {
-//     try {
-//         const user = await User.findById(req.params.id);
-//         const { password, ...others } = user._doc;
-//         res.status(200).json(others);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+// GET Product
+router.get("/find/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // // GET ALL USERs
