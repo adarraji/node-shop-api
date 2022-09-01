aac25e9d40cbdc0f15f77dd9e05aef85420c6dc1
adarraji <a.darraji@outlook.com>
2022-08-31 20:11:12 -0400

    fix

--- routes/product.js
+++ routes/product.js
@@ -53,7 +53,7 @@ router.get("/", async (req, res) => {
     const queryNew = req.query.new;
     const queryCategory = req.query.category;
     try {
-        let products = [];
+        let products;
 
         if (queryNew) {
             products - await Product.find().sort({ createdAt: -1 }).limit(5);
