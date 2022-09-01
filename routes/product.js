21518cd54baf282fdd6ad82e4eb4fc473c059cb0
adarraji <a.darraji@outlook.com>
2022-08-31 20:27:19 -0400

    fix

--- routes/product.js
+++ routes/product.js
@@ -73,32 +73,4 @@ router.get("/", async (req, res) => {
     }
 });
 
-// // GET USER STATS
-// router.get("/stats", veryifyTokenAndAdmin, async (req, res) => {
-
-//     const date = new Date();
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-//     try {
-//         const data = await User.aggregate([
-//             { $match: { createdAt: { $gte: lastYear } } },
-//             {
-//                 $project: {
-//                     month: { $month: "$createdAt" },
-//                 },
-//             },
-//             {
-//                 $group: {
-//                     _id: "$month",
-//                     total: { $sum: 1 }
-//                 }
-//             }
-//         ]);
-//         res.status(200).json(data);
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// })
-
-
 module.exports = router;
