03649a0a4f54f89e422ccb713b12ce0cb86071ce
adarraji <a.darraji@outlook.com>
2022-09-01 11:10:12 -0400

    fix

--- routes/order.js
+++ routes/order.js
@@ -63,7 +63,7 @@ router.get("/", veryifyTokenAndAdmin, async (req, res) => {
 router.get("/income", veryifyTokenAndAdmin, async (req, res) => {
     const date = new Date()
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-    const previousMonth = new Date(new Date.setMonth(lastMonth.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
     try {
         const income = await Order.aggregate([
             { $match: { createdAt: { $gte: previousMonth } } },
