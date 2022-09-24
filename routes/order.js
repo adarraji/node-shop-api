c0690307bb2c62ee28f06b53850ecbd94fc48cf1
adarraji <a.darraji@outlook.com>
2022-09-23 20:17:34 -0400

    updated order income

--- routes/order.js
+++ routes/order.js
@@ -61,25 +61,33 @@ router.get("/", veryifyTokenAndAdmin, async (req, res) => {
 
 // GET MONTHLY INCOME
 router.get("/income", veryifyTokenAndAdmin, async (req, res) => {
+    const productId = req.query.pid;
     const date = new Date()
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
     try {
         const income = await Order.aggregate([
-            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $match: {
+                    createdAt: { $gte: previousMonth },
+                    ...(productId && {
+                        products: { $elemMatch: { productId } },
+                    }),
+                },
+            },
             {
                 $project: {
                     month: { $month: "$createdAt" },
-                    sales: "$amount"
+                    sales: "$amount",
                 },
             },
             {
                 $group: {
                     _id: "$month",
-                    total: { $sum: "$sales" }
+                    total: { $sum: "$sales" },
                 },
             },
-        ])
+        ]);
         res.status(200).json(income);
     }
     catch (err) {
