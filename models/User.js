1513a53eeb605d8d0a77e467c542336dcb215c38
adarraji <a.darraji@outlook.com>
2022-09-25 00:36:50 -0400

    added img to User model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema(
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        isAdmin: { type: Boolean, default: false }
+        isAdmin: { type: Boolean, default: false },
+        img: { type: String, required: true }
     },
     { timestamps: true }
 );
