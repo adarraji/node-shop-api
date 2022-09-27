b5b8e7c733107fed0566b1856f0c2d5f7cb3a0e4
adarraji <a.darraji@outlook.com>
2022-09-27 17:34:03 -0400

    added img

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,8 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        img: req.body.img,
     });
 
     try {
